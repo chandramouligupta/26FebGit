59508f17c9f3ae37199df19a9930f8527f198942
Chandramouli <chandramouligupta50@gmail.com>
2019-02-27 00:20:53 +0530

    Modified MyProg.cpp

--- myprog.cpp
+++ myprog.cpp
@@ -6,11 +6,15 @@ int add(int a,int b){
 	return a+b;
 }
 
+int difference(int a,int b){
+	return a>b?a-b:b-a;
+}
+
 int main(int argc, char const *argv[])
 {
 	/* code */
 	int a,b;
 	cin>>a>>b;
-	cout<<"A+B = "<<add(a,b)<<"\n";
+	cout<<"A+B = "<<add(a,b)<<" A-B = "<<difference(a,b)<<"\n";
 	return 0;
 }
