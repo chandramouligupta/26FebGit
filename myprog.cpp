ff6bcd268ffce4047cd11ee3cdee46311a46ff3c
Chandramouli <chandramouligupta50@gmail.com>
2019-02-27 00:22:07 +0530

    master commit going to fail

--- myprog.cpp
+++ myprog.cpp
@@ -6,11 +6,15 @@ int add(int a,int b){
 	return a+b;
 }
 
+int multiply(int a,int b){
+	return a*b;
+}
+
 int main(int argc, char const *argv[])
 {
 	/* code */
 	int a,b;
 	cin>>a>>b;
-	cout<<"A+B = "<<add(a,b)<<"\n";
+	cout<<"A+B = "<<add(a,b)<<" A*B = "<<multiply(a,b)<<"\n";
 	return 0;
 }
